c6e815a4c6305dfcec5c2be781de1b5d44e43ee9
nasih04 <dev.nasih@gmail.com>
2024-12-19 23:39:39 +0530

    propTypes in react

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Student from "./Student.jsx";
 function App() {
   return (
     <>
-      <Student name="SpongeBOB" age={30} isStudent={true}/>
+      <Student name="SpongeBOB" age={30} isStudent={false}/>
       <Student name="Nasih" age={20} isStudent={true}/>
       <Student name="Mubashir" age={21} isStudent={true}/>
       <Student name="Shifin" age={20} isStudent={true}/>
--- src/Student.jsx
+++ src/Student.jsx
@@ -1,3 +1,4 @@
+import  PropTypes from 'prop-types';
 
 function Student(props){
     return (
@@ -7,6 +8,13 @@ function Student(props){
             <p>Student: {props.isStudent ? "Yes" : "No"}</p>
         </div>
     )
+}
+
+
+Student.propTypes = {
+    name: PropTypes.string,
+    age: PropTypes.number,
+    isStudent: PropTypes.bool
 }
 
 export default Student;
