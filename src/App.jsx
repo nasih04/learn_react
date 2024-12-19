7ea03a110448ed84c7bba6b7d083e3eb0efff717
nasih04 <dev.nasih@gmail.com>
2024-12-19 23:34:07 +0530

    props : student component

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
-import Card from "./Card";
+import Student from "./Student.jsx";
 
 function App() {
-
   return (
     <>
-      <Card />
-      <Card />
-      <Card />
-      <Card />
+      <Student name="SpongeBOB" age={30} isStudent={true}/>
+      <Student name="Nasih" age={20} isStudent={true}/>
+      <Student name="Mubashir" age={21} isStudent={true}/>
+      <Student name="Shifin" age={20} isStudent={true}/>
     </>
-  );
+  )
 }
 
 export default App
